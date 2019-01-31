25595485a4e7dc1bc1d0086103d057194381acb6
Altamore Quentin <quentin.altamore@estiam.com>
2019-01-31 01:06:32 +0100

    Et on finit le serveur

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ app.use(function (req, res, next) {
 
 //liste des topics disponibles
 app.get('/topics', function(req, res) {
-    console.log(forum)
-    res.send(forum);
+    console.log(topics)
+    res.send(topics);
 });
 
 //Liste des messages dans le topic :id
 app.get('/topics/:id', function(req, res) {
-    res.send("Oui")
+    var messages = getMessagesFromTopic(req.query.topicId)
+    console.log(messages)
+    res.send(messages)
 });
 
 //Contenu du message :mid dans le topic :id
@@ -31,47 +33,82 @@ app.get('/topics/:id/:mid', function(req, res) {
 });
 
 app.post('/topics/new', function(req, res) {
-    console.log("oui")
+    var topicsLength = topics.length
+    var title = req.query.title
+    var topicsNewLength = topics.push({"id": topicsLength+1, 
+                "title": title})
+    if(topicsLength < topicsNewLength)
+        res.send(true)
+    else
+        res.send(false)
 });
 
 //Nouveau message dans le topic :id
 app.post('/topics/:id/new', function(req, res) {
-    res.send("Oui")
+    var messagesLength = messages.length
+    var dateJ = new Date(Date.now()).toLocaleString()
+    var messagesNewLength = messages.push({'topicId':parseInt(req.query.topicId), 
+                    'messageId':messages.length+1,
+                    'content':req.query.messageContent,
+                    'date':dateJ})
+    console.log(messages)
+    if(messagesLength < messagesNewLength)
+        res.send(true)
+    else
+        res.send(false)
 });
 
-app.listen(port, "192.168.43.224",  () => console.log(`App listening on 192.168.43.224:${port}`));
+function getMessagesFromTopic(topicId) {
+    var messagesFromTopic = []
+    for(var i = 0; i < messages.length; i++) {
+        if(messages[i].topicId == topicId)
+            messagesFromTopic.push(messages[i])
+    }
+    return messagesFromTopic
+}
 
-var forum = [
+var topics = [
     {
-        id : "1",
-        title : "Quel est votre animal Préféré ?",
-        messages : [
-            {
-                id : "1",
-                content : "Moi j'adore les loups ! *.*"
-            },
-            {
-                id : "2",
-                content : "Moi les chats ! :O Better than everything omg :O"
-            },
-            {
-                id : "3",
-                content : "Moi j'aime bien Adama :D Sacrée bestiole !"
-            }
-        ]
+        id : 1,
+        title : "Quel est votre animal Préféré ?"
     },
     {
-        id : "2",
-        title : "Quel est votre professeur préféré ?",
-        messages : [
-            {
-                id : "1",
-                content : "Zino ofc !"
-            },
-            {
-                id : "2",
-                content : "Y a pas meilleur que Zino ! Qui oserait dire le contraire ? :O"
-            }
-        ]
+        id : 2,
+        title : "Quel est votre professeur préféré ?"
     }
 ];
+
+var messages = [
+    {
+        topicId : 1,
+        messageId : 1,
+        content : "Moi j'adore les loups ! *.*",
+        date : "2019-1-30 14:34:04"
+    },
+    {
+        topicId : 1,
+        messageId : 2,
+        content : "Moi les chats ! :O Better than everything omg :O",
+        date : "2019-1-30 14:35:04"
+    },
+    {
+        topicId : 1,
+        messageId : 3,
+        content : "Moi j'aime bien Adama :D Sacrée bestiole !",
+        date : "2019-1-30 14:36:04"
+    },
+    {
+        topicId : 2,
+        messageId : 4,
+        content : "Zino ofc !",
+        date : "2019-1-30 14:38:04"
+    },
+    {
+        topicId : 2,
+        messageId : 5,
+        content : "Y a pas meilleur que Zino ! Qui oserait dire le contraire ? :O",
+        date : "2019-1-30 14:40:04"
+    }
+]
+
+app.listen(port, "192.168.43.224",  () => console.log(`App listening on 192.168.43.224:${port}`));
